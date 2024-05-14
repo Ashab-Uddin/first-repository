4ec08d9ce50fc16afbba998425705a7b4f454471
Ashab-Uddin <ashabuddin47@gmail.com>
2024-05-15 01:49:38 +0600

    Initial commit

---
+++ s.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   int a,b;
+   printf("Enter a number: ");
+   scanf("%d",&a);
+   printf("Enter b number: ");
+   scanf("%d",&b);
+   printf("%d",a+b);
+   return 0;
+}
